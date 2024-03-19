2ffea449468afeb5fc3d2aafab41e9fff84cfd19
saurabh8423 <saurabhch9369@gmail.com>
2024-03-19 22:54:25 +0530

    Added about us page and fixed sidebar login signup button

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -2,9 +2,8 @@ import './App.css';
 
 import {Route, Routes } from 'react-router-dom';
 
-// import Footer from './Components/Footer.jsx';
-// import HomeLayout from './Layouts/HomeLayout.jsx';
 import HomePage from './Pages/HomePage.jsx';
+import AboutUs from './Pages/HomePage.jsx';
 
 function App() {
   
@@ -12,10 +11,12 @@ function App() {
     <>
         <Routes>
           <Route path ="/" element = {<HomePage />}></Route>
+          <Route path ="/about" element = {<AboutUs />}></Route>
+
+          
         </Routes>
 
-        {/* <Footer /> */}
-        {/* <HomeLayout /> */}
+        
     </>
   )
 }
