ba63402a3256f9c406be418ca400ee55407989b4
saurabh8423 <saurabhch9369@gmail.com>
2024-04-13 00:32:10 +0530

    Added complete signup flow

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import {Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage.jsx';
 import AboutUs from './Pages/HomePage.jsx';
 import NotFound from './Pages/NotFound.jsx';
+import Signup from './Pages/Signup.jsx';
 
 function App() {
   
@@ -14,6 +15,8 @@ function App() {
           <Route path ="/" element = {<HomePage />}></Route>
           <Route path ="/about" element = {<AboutUs />}></Route>
 
+          <Route path = "signup" element={<Signup />}></Route>
+          
           <Route path = "*" element={<NotFound />}></Route>
         </Routes>
 
--- lms-frontend/src/Redux/Slices/AuthSlice.js
+++ lms-frontend/src/Redux/Slices/AuthSlice.js
@@ -1,16 +1,34 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
 
+import axiosInstance from "../../Helpers/axiosinstance"
 const initialState = {
-    isLoggedIn : localStorage.getItem('isLoggedIn') || false,
+    isLoggedIn: localStorage.getItem('isLoggedIn') || false,
     role: localStorage.getItem('role') || "",
     data: localStorage.getItem('data') || {}
 };
 
-const authSlice= createSlice({
+export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
+    try {
+        const res = axiosInstance.post("user/register", data);
+        toast.promise(res, {
+            loading: "Wait! creating your account",
+            success: (data) => {
+                return data?.data?.message;
+            },
+            error: "Failed to create account"
+        });
+        return (await res).data;
+    } catch(error) {
+        toast.error(error?.response?.data?.message);
+    }
+})
+
+const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {},
 });
-
-// export const {}= authSlice.actions;
+// export const {} = authSlice.actions;
 export default authSlice.reducer;
