fa713b6b1dddc9b85a17126d16f7d8e16242e5a5
saurabh8423 <saurabhch9369@gmail.com>
2024-07-17 14:17:23 +0530

    Added require Auth

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import CourseList from "./Pages/Course/CourseList";
 import Contact from "./Pages/Contact";
 import Denied from "./Pages/Denied";
 import CourseDescription from "./Pages/Course/CourseDescription";
+import RequireAuth from "./Components/Auth/RequireAuth";
+import CreateCourse from "./Pages/Course/CreateCourse";
 
 function App() {
   return (
@@ -25,6 +27,10 @@ function App() {
 
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+
+        <Route element={<RequireAuth allowedRoles={["ADMIN"]} />}>
+        <Route path="/course/create" element={<CreateCourse />} />
+        </Route>
 
         <Route path="*" element={<NotFound />} />
       </Routes>
