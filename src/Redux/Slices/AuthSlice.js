480c7e0a69edf7714807eeef51d5f579a76e6eaf
saurabh8423 <saurabhch9369@gmail.com>
2024-07-26 22:46:28 +0530

    Added edit profile page

--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -6,7 +6,7 @@ import axiosInstance from "../../Helpers/axiosinstance"
 const initialState = {
     isLoggedIn: localStorage.getItem('isLoggedIn') || false,
     role: localStorage.getItem('role') || "",
-    data: JSON.parse(localStorage.getItem('data')) || {}
+    data: localStorage.getItem('data') != undefined ? JSON.parse(localStorage.getItem('data')) : {}
 };
 
 export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
@@ -55,7 +55,32 @@ export const logout = createAsyncThunk("/auth/logout", async () =>{
     } catch (error) {
         toast.error(error?.response?.data?.message); 
     }
+});
+
+export const updateProfile = createAsyncThunk("/user/update/profile", async (data) => {
+    try {
+        const res = axiosInstance.put(`user/update/${data[0]}`, data[1]);
+        toast.promise(res, {
+            loading: "Wait! profile update in progress...",
+            success: (data) => {
+                return data?.data?.message;
+            },
+            error: "Failed to update profile"
+        });
+        return (await res).data;
+    } catch(error) {
+        toast.error(error?.response?.data?.message);
+    }
 })
+
+export const getUserData = createAsyncThunk("/user/details", async () => {
+    try {
+        const res = axiosInstance.get("user/me");
+        return (await res).data;
+    } catch(error) {
+        toast.error(error.message);
+    }
+})    
 
 const authSlice = createSlice({
     name: 'auth',
@@ -76,6 +101,15 @@ const authSlice = createSlice({
             state.isLoggedIn =false;
             state.role =""; 
         })
+        .addCase(getUserData.fulfilled, (state, action) => {
+            if(!action?.payload?.user) return;
+            localStorage.setItem("data", JSON.stringify(action?.payload?.user));
+            localStorage.setItem("isLoggedIn", true);
+            localStorage.setItem("role", action?.payload?.user?.role);
+            state.isLoggedIn = true;
+            state.data = action?.payload?.user;
+            state.role = action?.payload?.user?.role
+        });
     }
 });
 // export const {} = authSlice.actions;
