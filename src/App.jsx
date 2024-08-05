58cdb518aad9074cd1ad6a7053ef92d3ccb72959
saurabh8423 <saurabhch9369@gmail.com>
2024-08-05 16:28:24 +0530

    Added course lectures

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import EditProfile from "./Pages/User/EditProfile";
 import Checkout from "./Pages/Payment/Checkout";
 import CheckoutSuccess from "./Pages/Payment/CheckoutSuccess";
 import CheckoutFailure from "./Pages/Payment/CheckoutFailure";
+import Displaylectures from "./Pages/Course/Dashboard/Displaylectures";
 
 function App() {
   return (
@@ -43,6 +44,7 @@ function App() {
           <Route path='/checkout' element={<Checkout />} />
           <Route path='/checkout/success' element={<CheckoutSuccess />} />
           <Route path='/checkout/fail' element={<CheckoutFailure />} />
+          <Route path='/course/displaylectures' element={<Displaylectures />}/>
         </Route>
 
         <Route path="*" element={<NotFound />} />
